3d010b3c522c4b979d04092cd6ee5c9beb9edcc5
Benjamin Nguyen <bengi3288@gmail.com>
2024-07-15 23:59:12 -0400

    Initial Commit

---
+++ HealthHomeKitMediator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HealthHomeKitMediator
+//
+//  Created by Ben Nguyen on 7/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HealthHomeKitMediator/HealthHomeKitMediatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthHomeKitMediatorApp.swift
+//  HealthHomeKitMediator
+//
+//  Created by Ben Nguyen on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthHomeKitMediatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
